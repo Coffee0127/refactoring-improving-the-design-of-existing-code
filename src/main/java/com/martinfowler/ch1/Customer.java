b42ba39066861bb77ec3fa7d637e0b51c4062df2
Bo-Xuan Fan <coffee0127@gmail.com>
2019-04-04 20:32:38 +0800

    1.3 Extract query method: getTotalFrequentRenterPoints

--- src/main/java/com/martinfowler/ch1/Customer.java
+++ src/main/java/com/martinfowler/ch1/Customer.java
@@ -20,13 +20,11 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;   // 常客積點
         Enumeration<Rental> rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
             Rental each = rentals.nextElement();    // 取得一筆租借紀錄
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental (顯示此筆租借資料)
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
@@ -34,7 +32,7 @@ public class Customer {
 
         // add footer lines (結尾列印)
         result += "Amount owed is " + getTotalCharge() + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
         return result;
     }
 
@@ -49,4 +47,14 @@ public class Customer {
         return totalAmount;
     }
 
+    // query method
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 }
