22edc98a5ed322b18dc76b2a38f1b883bf8e3a81
Bo-Xuan Fan <coffee0127@gmail.com>
2019-04-04 15:21:47 +0800

    1.1 起點

---
+++ src/main/java/com/martinfowler/ch1/Customer.java
@@ -0,0 +1,69 @@
+package com.martinfowler.ch1;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;                              // 姓名
+    private Vector<Rental> _rentals = new Vector<>();  // 租借紀錄
+
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;         // 總消費金額
+        int frequentRenterPoints = 0;   // 常客積點
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = rentals.nextElement();    // 取得一筆租借紀錄
+
+            // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:         // 普通片
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:     // 新片
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:       // 兒童片
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            // add frequent renter points (累加 常客積點)
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // show figures for this rental (顯示此筆租借資料)
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines (結尾列印)
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+}
---
+++ src/main/java/com/martinfowler/ch1/Movie.java
@@ -0,0 +1,27 @@
+package com.martinfowler.ch1;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;    // 名稱
+    private int _priceCode;   // 價格(代號)
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+}
