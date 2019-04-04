c29682201b7fd28fb0417f857ac548c3df35cb22
Bo-Xuan Fan <coffee0127@gmail.com>
2019-04-04 20:59:25 +0800

    1.4 Extract local variable: daysRented

--- src/main/java/com/martinfowler/ch1/Rental.java
+++ src/main/java/com/martinfowler/ch1/Rental.java
@@ -19,21 +19,22 @@ public class Rental {
 
     double getCharge() {     // 計算一筆租片費用
         Movie movie = getMovie();
+        int daysRented = getDaysRented();
         double result = 0;
         switch (movie.getPriceCode()) {
             case Movie.REGULAR:         // 普通片
                 result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:     // 新片
-                result += getDaysRented() * 3;
+                result += daysRented * 3;
                 break;
             case Movie.CHILDRENS:       // 兒童片
                 result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
                 }
                 break;
         }
